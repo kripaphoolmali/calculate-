30743b2f20d7a1888f010198bdce28b52dbed4d6
kripaphoolmali <kripaphoolmali@gmail.com>
2024-11-29 14:11:19 +0530

    CALSI V1

---
+++ cal.js
@@ -0,0 +1,77 @@
+
+var myinputbox = document.getElementById("inputbox")
+
+
+function input1(){
+    
+    myinputbox.value += "1"
+}
+function input2(){
+    myinputbox.value += "2"
+}
+function input3(){
+    myinputbox.value += "3"
+}
+function input4(){
+    myinputbox.value += "4"
+}
+function input5(){
+    myinputbox.value += "5"
+}
+function input6(){
+    myinputbox.value += "6"
+}
+function input7(){
+    myinputbox.value += "7"
+}
+function input8(){
+    myinputbox.value += "8"
+}
+function input9(){
+    myinputbox.value += "9"
+}
+function input0(){
+    myinputbox.value += "0"
+}
+
+function dev(){
+    myinputbox.value += "/"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function add(){
+    myinputbox.value += "+"
+}
+function sub(){
+    myinputbox.value += "-"
+}
+
+function per(){
+    myinputbox.value += "%"
+}
+
+function malti(){
+    myinputbox.value += "*"
+}
+
+function dot(){
+    myinputbox.value += "."
+}
+
+function result(){
+    myinputbox.value = eval(myinputbox.value)
+}
